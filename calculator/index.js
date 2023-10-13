a75898cfa24e411745d5ef1904a59a41a1107f69
Ahmad1126 <111472289+Ahmad1126@users.noreply.github.com>
2023-10-14 01:05:51 +0530

    Update index.js

--- calculator/index.js
+++ calculator/index.js
@@ -1,4 +1,3 @@
-// Get all the keys from document
 var keys = document.querySelectorAll('#calculator span');
 var operators = ['+', '-', 'x', '÷'];
 var decimalAdded = false;
@@ -11,7 +10,6 @@ for(var i = 0; i < keys.length; i++) {
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
 		
-		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
 		// If clear key is pressed, erase everything
 		if(btnVal == 'C') {
 			input.innerHTML = '';
@@ -22,11 +20,8 @@ for(var i = 0; i < keys.length; i++) {
 		else if(btnVal == '=') {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
-			
-			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
 			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
 			
-			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
 				equation = equation.replace(/.$/, '');
 			
@@ -47,16 +42,13 @@ for(var i = 0; i < keys.length; i++) {
 			else if(inputVal == '' && btnVal == '-') 
 				input.innerHTML += btnVal;
 			
-			// Replace the last operator (if exists) with the newly pressed operator
 			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
 				input.innerHTML = inputVal.replace(/.$/, btnVal);
 			}
 			
 			decimalAdded =false;
 		}
 		
-		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
 		else if(btnVal == '.') {
 			if(!decimalAdded) {
 				input.innerHTML += btnVal;
